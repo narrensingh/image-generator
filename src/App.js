73a53ba99d6f791b9076db5e271e46c61c4eac0a
narrensingh <narrensingh31@gmail.com>
2024-07-06 22:15:34 +0530

    base -v

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from'react'
+import { useState, useEffect } from 'react'
+import Imagecard from './components/Card'
+import ImageSearch from './components/ImageSearch'
 
-function App() {
+export default function App() {
+  const [images,setimages] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [term,setTerm] = useState('')
+
+
+  useEffect(() =>  {fetch(`https://pixabay.com/api/?key=44743096-bd43d4dbf3f14da3852874011&q=${term}&image_type=photo&pretty=true`)
+            .then(res => res.json())
+            .then(data => {
+              setimages(data.hits)
+              setIsLoading(false)
+            })
+            .catch(err => console.log(err))}
+          ,[term])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ImageSearch searchText={text => setTerm(text)}/>
+        {
+          !isLoading && images.length === 0 ? <h1 className='text-3xl text-center text-purple-600'>Sorry no items found 🙁.</h1> : <h1 className='text-3xl text-center text-purple-600 mb-6'>There are {images.length} images found for your search.</h1>
+        }
+      <div className='grid grid-cols-3 gap-4 mx-auto '>
+        {
+          isLoading ? <h1 className='text-3xl mr-auto text-purple-600'>Loading...</h1> : images.map(image => <Imagecard key={image.id} image={image}/>)
+        }
+      </div>
+    </>
+  )
 }
-
-export default App;
---
+++ src/components/ImageSearch.js
@@ -0,0 +1,24 @@
+import react from 'react'
+import { useState } from 'react'
+
+const ImageSearch = ({searchText}) => {
+    const [text,settext]=useState('')
+    const search = (e) => {
+        e.preventDefault();
+        searchText(text)
+    }
+    return (
+            <div className="relative w-80 min-w-[200px] h-10 m-auto mt-10 mb-10 flex items-end justify-center">
+                <form className="w-full max-w-sm" onSubmit={search}>
+                    <div className="flex items-center border-b border-purple-500 py-2">
+                        <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image" aria-label="search" onChange={(e) => settext(e.currentTarget.value)}/>
+                        <button className="flex-shrink-0 bg-purple-500 hover:bg-purple-700 border-purple-500 hover:border-purple-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+                        Search
+                        </button>
+                    </div>
+                </form>
+            </div>
+    )
+}
+
+export default ImageSearch
